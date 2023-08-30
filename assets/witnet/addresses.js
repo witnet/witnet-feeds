daed4aef5748837100a7340aae473d081d3639a1
Guillermo Díaz <guillermo@otherplane.com>
2023-08-30 12:34:07 +0200

    chore: deploy pfs on mantle.testnet

--- assets/witnet/addresses.js
+++ assets/witnet/addresses.js
@@ -2,6 +2,7 @@ const { merge } = require("lodash")
 module.exports = {
     ...merge(
         require("witnet-solidity/assets/witnet/addresses"),
+        require("witnet-solidity-bridge/migrations/witnet.addresses"),
         require("../../migrations/witnet/addresses")
     )
 };
