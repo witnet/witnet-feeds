98452cd190e0634c7705a828f84406a8f5ba8eb8
Guillermo Díaz <guillermo@witnet.io>
2025-11-20 09:24:50 +0100

    feat: pfs:check script

---
+++ scripts/rulebook.cjs
@@ -0,0 +1,38 @@
+const { assets, utils, Rulebook } = require("../dist/src/lib");
+
+const rulebook = Rulebook.default()
+
+let errors = 0, requests = 0
+const priceFeeds = rulebook.getNetworkPriceFeeds();
+const maxCaptionWidth = Math.max(...priceFeeds.requests.map(caption => caption.length))
+
+priceFeeds.requests
+    .sort()
+    .filter((caption, index) => priceFeeds.requests.indexOf(caption) === index)
+    .forEach((caption) => {
+        const artifact = utils.captionToWitOracleRequestPrice(caption);
+        let request;
+        try {
+            request = utils.requireRadonRequest(artifact, assets);
+            console.info(
+                `✅ ${caption}${" ".repeat(maxCaptionWidth - caption.length)} => [${request.radHash}] ${artifact}`
+            )
+            requests += 1
+        } catch (err) {
+            console.error(
+                `❌ ${caption}${" ".repeat(maxCaptionWidth - caption.length)} => ${err}`,
+            );
+            errors += 1
+        }
+        rulebook.getPriceFeedUpdateConditions(caption, true)
+        rulebook.getPriceFeedUpdateConditions(caption, false)
+    });
+
+console.info("-".repeat(80))
+if (errors) {
+    console.error(`^ Total errors: ${errors}`)
+    process.exit(1);
+} else {
+    console.error(`^ Total requests: ${requests}`)
+    process.exit(1);
+}
